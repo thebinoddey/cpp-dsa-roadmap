2d5219d3c0e03015360f116596307f80b7f626fb
Binod Dey <binoddey06@gmail.com>
2026-07-25 12:31:27 +0530

    Array Pointer in C++

---
+++ 01_CPP/03_Pointers/02_ArrayPointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1, 2, 3, 4};
+
+    //arr is a pointer which has value = arr[0]
+    cout << *arr << endl;
+
+    //arr is a constant pointer. It's value cannot be changed.
+
+
+}
