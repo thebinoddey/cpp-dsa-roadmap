af0aab952d66be72fb4c5187229b39d6bc5ec133
Binod Dey <binoddey06@gmail.com>
2026-09-19 22:36:43 +0530

    LC 48: RotateImage

---
+++ 04_DSAPractice/01_Arrays/2D Array/01_RotateImage_LC48.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        // Reverse every row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+            }
+        }
+        
+    }
+};
